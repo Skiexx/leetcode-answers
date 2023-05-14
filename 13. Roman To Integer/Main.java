7094412510260656e739386afa2e8a3e93d2617f
Skiexx <95953737+Skiexx@users.noreply.github.com>
2023-05-14 23:44:55 +0700

    Added answer to 13 task (#2)

---
+++ 13. Roman To Integer/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(romanToInt("MDXCVII"));
+	}
+
+	public static int romanToInt(String s) {
+		int answer = 0, number = 0, prev = 0;
+		for (int i = s.length() - 1; i >= 0; i--) {
+			switch (s.charAt(i)) {
+				case 'M' -> number = 1000;
+				case 'D' -> number = 500;
+				case 'C' -> number = 100;
+				case 'L' -> number = 50;
+				case 'X' -> number = 10;
+				case 'V' -> number = 5;
+				case 'I' -> number = 1;
+			}
+			if (number < prev) {
+				answer -= number;
+			} else {
+				answer += number;
+			}
+			prev = number;
+		}
+		return answer;
+	}
+}
